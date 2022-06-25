b2497c7e3a64a937f6a83286c0a1249fe68bfd2d
casey shimata <caseyshimata@10MISDEV7096.local>
2022-06-24 23:09:18 -0600

    Create a local Java Maven Spring REST project called ACHLibrary.

---
+++ src/test/java/com/example/achlibrary/AchLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.achlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

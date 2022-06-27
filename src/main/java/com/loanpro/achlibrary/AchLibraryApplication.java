8745e903e2eba1616c1229c06974fe08cfbc0b28
casey shimata <caseyshimata@10MISDEV7096.local>
2022-06-26 22:37:01 -0600

    Remove mapping functions to ValidatorRouter class for now.

--- src/main/java/com/loanpro/achlibrary/AchLibraryApplication.java
+++ src/main/java/com/loanpro/achlibrary/AchLibraryApplication.java
@@ -1,29 +1,13 @@
-package com.example.achlibrary;
+package com.loanpro.achlibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-public class AchLibraryApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(AchLibraryApplication.class);
-    }
+public class AchLibraryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AchLibraryApplication.class, args);
     }
 
-    @GetMapping("/validateACH")
-    public String validateACH() {
-        String fileSummary = "Here is the file summary.";
-        return fileSummary;
-    }
 }

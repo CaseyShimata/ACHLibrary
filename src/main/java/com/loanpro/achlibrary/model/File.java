f6e8070ee19676d073c1d8dec98be1eccd1eb228
casey shimata <caseyshimata@10MISDEV7096.local>
2022-06-26 22:29:10 -0600

    LOANPRO-21: Create a parent class with properties getters and setters for an ACHFile.

--- src/main/java/com/loanpro/achlibrary/model/File.java
+++ src/main/java/com/loanpro/achlibrary/model/File.java
@@ -1,8 +1,36 @@
-package com.loanpro.achlibrary.validatorstructure.model;
+package com.loanpro.achlibrary.model;
+
+import java.util.ArrayList;
 
 public class File {
+    private String rawFile;
+    private ArrayList<Record> records = new ArrayList<Record>();
 
+    public File() {
 
+    }
 
+    public String getRawFile() {
+        return rawFile;
+    }
 
+    public void setRawFile(String rawFile) {
+        this.rawFile = rawFile;
+    }
+
+    public ArrayList<Record> getRecords() {
+        return records;
+    }
+
+    public Record getRecordAtIndex(int index){
+        return this.records.get(index);
+    }
+
+    public void setRecords(ArrayList<Record> records) {
+        this.records = records;
+    }
+
+    public void appendToRecords(Record record){
+        this.records.add(record);
+    }
 }
